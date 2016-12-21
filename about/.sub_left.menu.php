45562b41174764798af1600f472b588367dee641
Andrew Abalakov <a.s.abalakov@ucre.ru>
2016-12-21 17:30:18 +0500

    Добавил пункты меню и сами страницы - Политика конфиденциальности и список используемых терминов

---
+++ about/.sub_left.menu.php
@@ -0,0 +1,39 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Достижения", 
+		"/about/awards/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Сертификаты", 
+		"/about/certificates/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Деятельность застрахована", 
+		"/about/deyatelnost-zastrakhovana", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Политика конфиденциальности", 
+		"/about/privacy", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Основные термины политики конфиденциальности", 
+		"/about/terms/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
