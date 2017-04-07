d63a6a9ed42da48fe4fe0575617d068ba7aad037
Andrew Abalakov <a.s.abalakov@ucre.ru>
2017-04-07 17:13:23 +0500

    +счетчик mail.ru

--- bitrix/templates/ittian-realty/footer.php
+++ bitrix/templates/ittian-realty/footer.php
@@ -97,5 +97,21 @@
 <!-- Онлайн чат корпоративного портала -->
 <link rel="stylesheet" href="https://cdn.callbackkiller.com/widget/cbk.css">
 <script type="text/javascript" src="https://cdn.callbackkiller.com/widget/cbk.js?wcb_code=6d2c8fa41f50f9ec01177bb28c659612" charset="UTF-8" async></script>
+<!-- Rating@Mail.ru counter -->
+<script type="text/javascript">
+  var _tmr = window._tmr || (window._tmr = []);
+  _tmr.push({id: "2889161", type: "pageView", start: (new Date()).getTime()});
+  (function (d, w, id) {
+    if (d.getElementById(id)) return;
+    var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+    ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
+    var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+    if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+  })(document, window, "topmailru-code");
+</script><noscript><div>
+<img src="//top-fwz1.mail.ru/counter?id=2889161;js=na" style="border:0;position:absolute;left:-9999px;" alt="" />
+</div></noscript>
+<!-- //Rating@Mail.ru counter -->
+
 </body>
 </html>
